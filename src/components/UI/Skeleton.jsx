2b005f3f99995226e3cd1ffae467bbfe99b8bef3
DonteCarter11 <dontecarter269@gmail.com>
2025-09-05 07:19:41 -0400

    updated skeleton component

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -1,72 +1,87 @@
 import React from "react";
 
-const Skeleton = ({
-  width = "100%",
-  height = "20px",
-  borderRadius = "4px",
-}) => {
+const Skeleton = () => {
   return (
     <div
-      className="skeleton-box"
-      style={{
-        width,
-        height,
-        borderRadius,
-      }}
-    >
-      <div className="itm">
-        <div className="nft__item">
-          <div className="author_list_pp">
-            <div
-              style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: "#e0e0e0",
-                borderRadius: "50%",
-                animation: "pulse 1.5s ease-in-out infinite alternate",
-              }}
-            />
-          </div>
-          <div
-            style={{
-              backgroundColor: "#e0e0e0",
-              height: "20px",
-              marginBottom: "10px",
-              animation: "pulse 1.5s ease-in-out infinite alternate",
-            }}
-          />
-          <div className="nft__item_wrap">
-            <div
-              style={{
-                width: "100%",
-                height: "200px",
-                backgroundColor: "#e0e0e0",
-                animation: "pulse 1.5s ease-in-out infinite alternate",
-              }}
-            />
-          </div>
-          <div className="nft__item_info">
-            <div
-              style={{
-                height: "20px",
-                backgroundColor: "#e0e0e0",
-                marginBottom: "10px",
-                animation: "pulse 1.5s ease-in-out infinite alternate",
-              }}
-            />
-            <div
-              style={{
-                height: "16px",
-                backgroundColor: "#e0e0e0",
-                width: "60%",
-                animation: "pulse 1.5s ease-in-out infinite alternate",
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+              // key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <div
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      backgroundColor: "#f0f0f0",
+                      borderRadius: "50%",
+                      animation: "pulse 1.5s ease-in-out infinite alternate",
+                    }}
+                  />
+                </div>
+                <div
+                  className="de_countdown"
+                  style={{
+                    backgroundColor: "#f0f0f0",
+                    height: "20px",
+                    animation: "pulse 1.5s ease-in-out infinite alternate",
+                    marginBottom: "10px",
+                  }}
+                />
+
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <div
+                        style={{
+                          backgroundColor: "#f0f0f0",
+                          height: "30px",
+                          borderRadius: "4px",
+                          animation:
+                            "pulse 1.5s ease-in-out infinite alternate",
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "200px",
+                      backgroundColor: "#f0f0f0",
+                      animation: "pulse 1.5s ease-in-out infinite alternate",
+                    }}
+                  />
+                </div>
+                <div className="nft__item_info">
+                  <div
+                    style={{
+                      height: "20px",
+                      backgroundColor: "#f0f0f0",
+                      marginBottom: "10px",
+                      animation: "pulse 1.5s ease-in-out infinite alternate",
+                    }}
+                  />
+                  <div
+                    style={{
+                      height: "16px",
+                      backgroundColor: "#f0f0f0",
+                      width: "60%",
+                      marginBottom: "8px",
+                      animation: "pulse 1.5s ease-in-out infinite alternate",
+                    }}
+                  />
+                  <div
+                    style={{
+                      height: "16px",
+                      backgroundColor: "#f0f0f0",
+                      width: "40px",
+                      animation: "pulse 1.5s ease-in-out infinite alternate",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
   );
 };
 
-export { Skeleton };
+export default Skeleton;
