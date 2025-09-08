3fe2abdc1d2c2dcfff3f857b49b7f3e41175c76f
DonteCarter11 <dontecarter269@gmail.com>
2025-09-08 15:16:02 -0400

    cleaned up links on the different pages to make sure everything routed properly

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -76,7 +76,7 @@ const ExploreItems = () => {
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
+                    to={`/author/${profile.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
                   >
@@ -109,7 +109,7 @@ const ExploreItems = () => {
                       </div>
                     </div>
                   </div>
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${profile.nftId}`}>
                     <img
                       src={profile.nftImage}
                       className="lazy nft__item_preview"
@@ -118,7 +118,7 @@ const ExploreItems = () => {
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${profile.nftId}`}>
                     <h4>{profile.title}</h4>
                   </Link>
                   <div className="nft__item_price">{profile.price} ETH</div>
